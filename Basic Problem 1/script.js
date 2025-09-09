56d973e32485d01ddedda6e486bb2261e536137b
nuhailfayez <nuhailfayez@gmail.com>
2025-09-09 22:56:12 +0600

    Basic problem 1

---
+++ Basic Problem 1/script.js
@@ -0,0 +1,90 @@
+// 1. Create a variable called carName, assign the value Volvo to it.
+
+let carName = 'Volvo';
+
+//  2. On one single line, declare three variables with the following names and values:
+
+// Ans:-
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+// So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+// 3rd Variable Name And Values Both! 
+
+// Ans:-
+// let firstName = "John";
+// let lastName = "Doe";
+// let age = 35;
+
+
+// 3.Use the correct assignment operator that will 
+// result in x being 50 (same as x = x * y). 
+
+// Ans :-
+// x = 10;
+// y = 5;
+
+// x *= y;
+
+
+// 4. Use comments to describe the correct data type of the following variables:
+
+let length = 16 // Number type
+let lastName = 'Johnson' // string type
+
+const x = {
+    firstName: 'John',
+    lastName: 'Doe'
+}  // object type
+
+
+// 5. Execute the function named myFunction.
+
+// function myFunction(){
+//    alert('Hello world')
+
+// }
+// myFunction()
+
+
+// 6.Create an object called person with name = John, age = 50, Then, access the object to alert("John is 50").
+
+// let person = {
+//     name : 'John',
+//     age: 50
+// }
+
+// alert(person.name + ' is ' + person.age);
+
+
+// 7. Array Related Question 
+// 1. Alert the number of items in an array, using the correct Array property: 
+
+// const cars = ['Volvo', 'Jeep', 'Mercedes']
+// alert(cars.length);
+
+// 2. Change the first item of Brand to "Ford".
+
+// const Brand = ['Volvo', 'Jeep', 'Mercedes']
+// Brand[0] = 'Ford';
+// console.log(Brand);
+
+
+// 8. Math Related Problems
+
+// 1. Use the correct Math method to create a random number.
+console.log(Math.random());
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+console.log(Math.max(10, 20));
+
+// 3.  Use the correct Math method to get the square root of 9.
+console.log(Math.sqrt(9))
+
+// 9. comparison operator related problems! 
+//  1. Choose the correct comparison operator to alert true, when x is greater than y.
+
+x = 10;
+y = 5;
+alert(x > y);
